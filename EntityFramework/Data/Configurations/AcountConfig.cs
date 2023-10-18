d9d1dc20c3cf9d5ce7ee4747e439ce9621168f58
Eg0r1k <egoruchstroganov07@gmail.com>
2023-10-18 20:32:19 +0300

    create database

---
+++ EntityFramework/Data/Configurations/AcountConfig.cs
@@ -0,0 +1,22 @@
+﻿using EntityFramework.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntityFramework.Data.Configurations
+{
+    public class AcountConfig : IEntityTypeConfiguration<Acount>
+    {
+        public void Configure(EntityTypeBuilder<Acount> builder)
+        {
+            builder.HasKey(x => x.Id);
+            builder.Property(x => x.Login).IsRequired();
+          
+          
+        }
+    }
+}
---
+++ EntityFramework/Data/Configurations/IncoumConfig.cs
@@ -0,0 +1,21 @@
+﻿using EntityFramework.Entities;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntityFramework.Data.Configurations
+{
+    internal class IncoumConfig : IEntityTypeConfiguration<Incoum>
+    {
+        public void Configure(EntityTypeBuilder<Incoum> builder)
+        {
+            builder.HasKey(x => x.Id);
+            builder.Property(x => x.Name).IsRequired();
+            builder.HasOne(x => x.Acounts).WithMany(x => x.Incoums).HasForeignKey(x => x.AcountId);
+        }
+    }
+}
