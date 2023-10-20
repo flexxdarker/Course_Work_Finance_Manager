b81f36b5d38fa2a61e4dfb70590cb854d48e923e
Андрій Грицюк <venganpro12@gmail.com>
2023-10-20 18:17:36 +0300

    add limits

--- EntityFramework/Entities/Cost.cs
+++ EntityFramework/Entities/Cost.cs
@@ -11,6 +11,7 @@ namespace EntityFramework.Entities
         public int Id { get; set; }
         public string Name { get; set; }
         public decimal Price { get; set; }
+        public decimal Limits { get; set; }
         public int CategoryId { get; set; }
         public virtual Category Category { get; set; }
       
