1055537da1abdeea3bac599f6e40808148bab13c
Strel_ba60@student.itstep.org <Strel_ba60@student.itstep.org>
2023-10-27 20:43:15 +0300

    done sort in detail window

--- FinanceManager/ShowDetailsOfType.xaml.cs
+++ FinanceManager/ShowDetailsOfType.xaml.cs
@@ -1,4 +1,6 @@
-﻿using EntityFramework.Repositories;
+﻿using EntityFramework.Entities;
+using EntityFramework.Repositories;
+using PropertyChanged;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -15,38 +17,58 @@ using System.Windows.Shapes;
 
 namespace FinancingManager
 {
-    /// <summary>
-    /// Interaction logic for ShowDetailsOfType.xaml
-    /// </summary>
+	/// <summary>
+	/// Interaction logic for ShowDetailsOfType.xaml
+	/// </summary>
+	/// 
+	[AddINotifyPropertyChangedInterface()]
     public partial class ShowDetailsOfType : Window
     {
         UnitOfWork UoW = new UnitOfWork();
-        public ShowDetailsOfType()
+        string category;
+		private void FillListBox(string category)
+		{
+			foreach (var item in UoW.CostRepo.Get(includeProperties: "Category").Where(x => x.Category.Name == category))
+			{
+				listBox.Items.Add(item);
+			}
+		}
+
+		public ShowDetailsOfType()
+        {
+			InitializeComponent();
+			category = "Health";
+			FillListBox(category);
+		}
+
+		public ShowDetailsOfType(string? Category)
         {
             InitializeComponent();
+			category = Category ?? "Health";
+			FillListBox(category);
+		}
 
-            
-        }
-
-        private void byPrice_Selected(object sender, RoutedEventArgs e)
+		private void byPrice_Selected(object sender, RoutedEventArgs e)
         {
-            
-        }
-
-        private void byDate_Selected(object sender, RoutedEventArgs e)
-        {
-
+            listBox.Items.Clear();
+            foreach(var item in UoW.CostRepo.Get(includeProperties: "Category").Where(x => x.Category.Name == category).OrderBy(x=>x.Price))
+            {
+                listBox.Items.Add(item);
+            }
         }
 
         private void byName_Selected(object sender, RoutedEventArgs e)
         {
-
-        }
-
+			listBox.Items.Clear();
+			foreach (var item in UoW.CostRepo.Get(includeProperties: "Category").Where(x => x.Category.Name == category).OrderBy(x => x.Name))
+			{
+				listBox.Items.Add(item);
+			}
+		}
         private void deleteBtn_Click(object sender, RoutedEventArgs e)
         {
-            
-
-        }
+			UoW.CostRepo.Delete((listBox.SelectedItem as Cost).Id);
+			UoW.Save();
+		}
     }
 }
